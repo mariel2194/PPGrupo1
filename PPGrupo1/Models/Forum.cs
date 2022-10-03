4070fd28cab28da94c7f0c04962b10e3e5c08273
Scarlet Gonzalez <smgonzalez@neural.com.do>
2022-10-03 17:51:14 -0400

    Fluent causes issues, solve that just commeting

--- PPGrupo1/Models/Forum.cs
+++ PPGrupo1/Models/Forum.cs
@@ -1,28 +1,28 @@
-//// -----------------------------------------------------------------------
-//// <copyright file="Forum.cs" company="Fluent.Infrastructure">
-////     Copyright © Fluent.Infrastructure. All rights reserved.
-//// </copyright>
-////-----------------------------------------------------------------------
-/// This is a test file created by Fluent Infrastructure in order to 
-/// illustrate its operation.
-/// See more at: https://github.com/dn32/Fluent.Infrastructure/wiki
-////-----------------------------------------------------------------------
+////// -----------------------------------------------------------------------
+////// <copyright file="Forum.cs" company="Fluent.Infrastructure">
+//////     Copyright © Fluent.Infrastructure. All rights reserved.
+////// </copyright>
+//////-----------------------------------------------------------------------
+///// This is a test file created by Fluent Infrastructure in order to 
+///// illustrate its operation.
+///// See more at: https://github.com/dn32/Fluent.Infrastructure/wiki
+//////-----------------------------------------------------------------------
 
-using Fluent.Infrastructure.FluentAttribute;
-using Fluent.Infrastructure.FluentModel;
-using System;
+//using Fluent.Infrastructure.FluentAttribute;
+//using Fluent.Infrastructure.FluentModel;
+//using System;
 
-namespace PPGrupo1.Models
-{
-    [FluentMapTo(typeof(ForumForm))]
-    public class Forum : BaseEntityClass
-    {
-        public string Description { get; set; }
-        public DateTime UpdateDate { get; set; }
-    }
+//namespace PPGrupo1.Models
+//{
+//    [FluentMapTo(typeof(ForumForm))]
+//    public class Forum : BaseEntityClass
+//    {
+//        public string Description { get; set; }
+//        public DateTime UpdateDate { get; set; }
+//    }
 
-    public class ForumForm : BaseEntityClass
-    {
-        public string Name { get; set; }
-    }
-}
+//    public class ForumForm : BaseEntityClass
+//    {
+//        public string Name { get; set; }
+//    }
+//}
